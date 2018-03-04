c26430620cbdb68144a9f6a34a4dc55a439b2d52
mgamez <mgamez@grupo-ari.net>
2018-03-04 04:42:51 -0400

    [Search] Country in Continents searching

--- continents/urls.py
+++ continents/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from continents.views import ContinentsView, ContinentsDetailIDView, ContinentsEachOneView
+from continents.views import (ContinentsView,
+                              ContinentsDetailIDView,
+                              ContinentsEachOneView,
+                              ContinentsSearchView)
 
 app_name = "continents"
 
@@ -7,4 +10,5 @@ urlpatterns = [
     path("", ContinentsView.as_view(), name="home"),
     path("<int:id>/", ContinentsDetailIDView.as_view(), name="continents_id_detail"),
     path("each_one/", ContinentsEachOneView.as_view(), name="each"),
+    path("each_one/<type>/<query>/",ContinentsSearchView.as_view(), name="search")
 ]
--- continents/views.py
+++ continents/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import (TemplateView,)
+from django.views.generic.list import ListView
+from countries.models import Country
 # Create your views here.
 class ContinentsView(TemplateView):
     template_name = "continents/continents.html"
@@ -21,3 +23,19 @@ class ContinentsEachOneView(TemplateView):
 
         return {'continents': continents}
     """
+class ContinentsSearchView(ListView):
+    template_name = 'continents/search.html'
+    model = Country
+
+    def get_queryset(self):
+        type_ = self.kwargs['type']
+        query_ = self.kwargs['query']
+
+        if type_ == 'contains':
+            return Country.objects.filter(name__contains=query_ )
+        elif type_ == 'icontains':
+            return Country.objects.filter(name__icontains=query_)
+        elif type == 'start':
+            return Country.objects.filter(name__startswith=query_)
+        else:
+            return Country.objects.filter(name=query_)
